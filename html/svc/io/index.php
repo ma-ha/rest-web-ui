7dca99fcb96c335c661fa52fb22a3c141407bfae
mh <m.e.harms@t-online.de>
2015-10-07 20:23:20 +0200

    IO: pump scenario "simulation"

--- html/svc/io/index.php
+++ html/svc/io/index.php
@@ -25,22 +25,12 @@ if ( ! isset( $_SESSION["p1"] ) ) {
 	
 }
 
-if ( $_SESSION["ledTMP"] == '2' ) {
-	if ( $_SESSION["switchTMP"]  == 'OFF') { // Power down done
-		$_SESSION["ledTMP"] = '-1';
-	} else {
-		if ( $_SESSION["switchRP"]  == 'ON') { // Power up done if PR is ON
-			$_SESSION["ledTMP"] = '1';
-		}
-	}
-}
-
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-	
-	error_log( $_POST['id'] .'='. $_POST['value'] );	
+
+	error_log( $_POST['id'] .'='. $_POST['value'] );
 	$_SESSION[ $_POST['id'] ] = $_POST['value'];
-	
+
 	if ( $_POST['id'] == 'switchTMP' ) {
 		if ( $_POST['value'] == 'ON' ) {
 			$_SESSION["ledTMP"] = '2';
@@ -74,14 +64,65 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 			$_SESSION["ledValveChHi"] = '-1';
 			$_SESSION["ledValveChLo"] = '1';
 		} else if ( $_POST['value'] == 'V4' ) {
-			$_SESSION["ledValveChLo"] = '-1';
-			$_SESSION["ledValveChHi"] = '1';
+			if ( $_SESSION["switchValveInflate"] < 10) {
+				$_SESSION["ledValveChLo"] = '-1';
+				$_SESSION["ledValveChHi"] = '1';
+			} else {
+				$_SESSION["switchValveChHi"] = 'inflate';
+			}
 		} else {
 			$_SESSION["ledValveChLo"] = '-1';
 			$_SESSION["ledValveChHi"] = '-1';
 		}
 	}
 }
+
+
+
+
+if ( $_SESSION["ledTMP"] == '2' ) {
+	if ( $_SESSION["switchTMP"]  == 'OFF') { // Power down done
+		$_SESSION["ledTMP"] = '-1';
+	} else {
+		if ( $_SESSION["switchRP"]  == 'ON' && $_SESSION["switchValveTMP"] == 'open') { // Power up done if PR is ON
+			$_SESSION["ledTMP"] = '1';
+		}
+	}
+}
+if ( $_SESSION["ledTMP"]  == '1') {
+	$_SESSION["p1"] = 2;
+	error_log( "p1 = 2" );
+} else {
+	if ( $_SESSION["ledValveTMP"] == '1' && $_SESSION["switchRP"] == 'ON') {
+		$_SESSION["p1"] = 10;
+		error_log( "p1 = 10" );
+	} else {
+		$_SESSION["p1"] = 100;
+		error_log( "p1 = 100" );
+	}
+}
+if ( $_SESSION["ledValveChHi"] == '1' ) {	
+	$_SESSION["p2"] = $_SESSION["p1"];
+}
+if ( $_SESSION["switchRP"] == 'ON') { 
+	if ( $_SESSION["switchValveInflate"] < 10) {
+		if ( $_SESSION["ledValveChLo"] == '1' ) {
+			$_SESSION["p2"] = 10;
+		} 
+		if ( $_SESSION["ledValveChHi"] == '1' ) {
+			$_SESSION["p2"] = $_SESSION["p1"];
+		} 
+	} else {
+		if ( $_SESSION["ledValveChLo"] == '1' ) {
+			$_SESSION["p2"] = 80;
+		} else if ( $_SESSION["ledValveChHi"] == '1' ) {
+			$_SESSION["p2"] = $_SESSION["p1"];
+		} else {
+			$_SESSION["p2"] = 100;
+		}
+	}
+}
+
 
 $data = array();
 $data['switchMain'] = array( 'value' => $_SESSION["switchMain"] );
@@ -92,7 +133,7 @@ $data['ledValveChLo'] = array( 'value' => $_SESSION["ledValveChLo"] );
 $data['ledTMP'] = array( 'value' => $_SESSION["ledTMP"] );
 $data['ledRP'] = array( 'value' => $_SESSION["ledRP"] );
 $data['displayG1'] = array( 'value' => $_SESSION["p1"].' kPa' );
-$data['displayG2'] = array( 'value' => $_SESSION["p1"].' kPa' );
+$data['displayG2'] = array( 'value' => $_SESSION["p2"].' kPa' );
 $data['switchRP']  = array( 'value' => $_SESSION["switchRP"] );
 $data['switchTMP'] = array( 'value' => $_SESSION["switchTMP"] );
 $data['switchValveN2']   = array( 'value' => $_SESSION["switchValveN2"]  );
