b3076e9f442b8c9308c26f53ebde622485875a0d
mh <m.e.harms@t-online.de>
2015-10-07 19:13:30 +0200

    IO plug in: switch + poti

--- html/modules/pong-io/pong-io.js
+++ html/modules/pong-io/pong-io.js
@@ -23,13 +23,14 @@ THE SOFTWARE.
 */
 
 log( "pong-io", "load module"); // print this on console, when module is loaded
-
+var ioSense = new Array();
 
 // ======= Code for "loadResourcesHtml" hook ================================================
 function pongIoDivHTML( divId, resourceURL, paramObj ) {
 	log( "pong-io",  "divId="+divId+" resourceURL="+resourceURL );
+	ioSense[ divId ] = new Array();
 	if ( moduleConfig[ divId ] != null ) {
-		pongIoRenderHTML( divId, resourceURL, paramObj, moduleConfig[ divId ]  );
+		pongIoRenderHTML( divId, resourceURL, paramObj, moduleConfig[ divId ] );
 	} else {
 		$.getJSON( 
 			resourceURL+"/pong-io", 
@@ -57,69 +58,164 @@ function pongIoRenderHTML( divId, resourceURL, paramObj, pmd ) {
 	log( "pong-io", w+"px/"+h+"px" );
 	contentItems.push( '<canvas id="'+divId+'Canvas" width="'+w+'px" height="'+h+'px">' );
 	contentItems.push( '</canvas></div>' );
-	$( "#"+divId ).html( contentItems.join( "\n" ) );
+	
+	$( "#"+htmlDivId ).html( contentItems.join( "\n" ) );
 
 	var ctx = document.getElementById( divId+'Canvas' ).getContext("2d");
-	// Make sure the image is loaded first otherwise nothing will draw.
+
 	if ( pmd.imgURL != null ) {
 		var background = new Image();
 		background.src = pmd.imgURL;
 		ctx.drawImage( background, 0, 0 );   
 	}
 	
-	pongIoUpdateData( divId, null );
-	
+	pongIoUpdateData( divId, { makeJS: true } );
+		
 	// output
 	log( "pong-io", "pongIoRenderHTML end.");
 
 }
 
+function pongIOmakeJS( divId  ) {
+	log( "pong-io", "pongIOmakeJS "+divId );
+	var pmd = moduleConfig[ divId ];
+	var contentItems = [];
+	contentItems.push( '<script>' );
+	contentItems.push( '  $( function() { ' );
+	contentItems.push( '    $( "#' +divId+ 'Canvas" ).click( function( e ) { ' );
+	contentItems.push( '      var x = e.pageX - $( "#' +divId+ 'Canvas" ).offset().left; ' );
+	contentItems.push( '      var y = e.pageY - $( "#' +divId+ 'Canvas" ).offset().top; ' );
+	//contentItems.push( '      alert( x+" / "+y ); ' );
+	if ( pmd.io && pmd.io.length ) {
+		
+		for ( var c = 0; c < pmd.io.length; c++ ) {
+			var io = pmd.io[c];
+			if ( io.type ) {
+				
+				if ( io.type == 'Switch' && io.values && io.values.length ) {
+
+					log( "pong-io", io.type+' '+io.id );			
+					for ( var val = 0; val < io.values.length; val++ ) {
+						if ( ioSense[ divId ] && ioSense[ divId ][ io.id ] && ioSense[ divId ][ io.id ][ io.values[val] ] ) {
+							//log( "pong-io", "sense ("+io.values[val] +")..." );
+							var s = ioSense[ divId ][ io.id ][ io.values[val] ];
+							//log( "pong-io", "sense "+ JSON.stringify(s) );
+							if ( s.x1 && s.x2 && s.y1 && s.y2 ) {
+								//log( "pong-io", "sense: " + JSON.stringify( s ) );
+								contentItems.push( '      pongIOcheckSwitchSense( "'+divId+'", x, y, "'+io.id+'", "'+io.values[val]+'",'+JSON.stringify( s )+' ); ' );
+							}
+						}
+					}
+					
+				} else if ( io.type == 'Poti' ) {
+
+					log( "pong-io", io.type+' '+io.id );		
+					if ( ioSense[ divId ] && ioSense[ divId ][ io.id ] ) {
+						var s = ioSense[ divId ][ io.id ];
+						contentItems.push( '      pongIOcheckPotiSense( "'+divId+'", x, y, "'+io.id+'", '+JSON.stringify( s )+' ); ' );						
+					}
+					
+				}
+			}
+		}
+	}
+	contentItems.push( '  } ) } );' );
+	contentItems.push( '</script>' );
+	$( "#"+divId+'pong-io_Div' ).append( contentItems.join( "\n" ) );
+	log( "pong-io", "pongIOmakeJS end." );
+}
+
+function pongIOcheckPotiSense( divId, x, y, id, s ) {
+	if ( ( x > s.x1 ) && ( x < s.x2 ) && ( y > s.y1 ) && ( y < s.y2 ) ) {
+		var val = s.min + ( x - s.x1 ) * ( s.max - s.min ) / ( s.x2 - s.x1 );
+		log( "pong-io", "Poti: "+id +" >> "+val );
+		$.ajax( 
+			{ url: moduleConfig[ divId ].dataURL, 
+			  type: "POST", 
+			  dataType: "json",
+			  data: { id:id, value:val, type:"Poti" }
+			}
+		).done(
+			function( dta) {
+				pongIOrenderData( divId, dta );
+			}
+		);
+	}
+}
+
+function pongIOcheckSwitchSense( divId, x, y, id, val, s ) {
+	if ( ( x > s.x1 ) && ( x < s.x2 ) && ( y > s.y1 ) && ( y < s.y2 ) ) {
+		log( "pong-io", "Switch: "+id+" >> "+val );
+		$.ajax( 
+			{ url: moduleConfig[ divId ].dataURL, 
+			  type: "POST", 
+			  dataType: "json",
+			  data: { id:id, value:val, type:"Switch" }
+			}
+		).done(
+			function( dta) {
+				pongIOrenderData( divId, dta );
+			}
+		);		  
+	}
+}
 
 /** update data call back hook */
 function pongIoUpdateData( divId, paramsObj ) {
 	log( "pong-io", "pongIoUpdateData start ");
-	var pmd = moduleConfig[ divId ];
 	
 	$.getJSON( 
-		pmd.dataURL, 
+		moduleConfig[ divId ].dataURL, 
 		function( dta ) {
-			
 			log( "pong-io", "pongIoUpdateData data start");
-			var ctx = document.getElementById( divId+'Canvas' ).getContext("2d");
-			if ( pmd.io && pmd.io.length ) {
-				log( "pong-io", "IO: "+ pmd.io.length);			
-				for ( var c = 0; c < pmd.io.length; c++ ) {
-					var io = pmd.io[c];
-					log( "pong-io", io.type+' '+io.id );			
-					var ioDta = null;
-					if ( dta && dta[ io.id ] ) {
-						ioDta = dta[ io.id ];
-					}
-					if ( io.type && io.pos && io.pos.x && io.pos.y ) {
-						
-						if ( io.type == 'LED') {
-							pongIOrenderLED( ctx, io, ioDta );
-						} else if ( io.type == 'Switch') {
-							pongIOrenderSwitch( ctx, io, ioDta );
-						} else if ( io.type == 'Poti') {
-							pongIOrenderPoti( ctx, io, ioDta );
-						} else if ( io.type == 'Gauge') {
-							pongIOrenderGauge( ctx, io, ioDta );
-						} else if ( io.type == 'Display') {
-							pongIOrenderDisplay( ctx, io, ioDta );
-						} else if ( io.type == 'Graph') {
-							pongIOrenderGraph( ctx, io, ioDta );
-						} else if ( io.type == 'Img') {
-							pongIOrenderImg( ctx, io, ioDta );
-						}  
-					}
-				}		
+					
+			pongIOrenderData( divId, dta );
+			
+			if ( paramsObj && paramsObj.makeJS ) {
+				pongIOmakeJS( divId );				
 			}
+	
 			log( "pong-io", "pongIoUpdateData data end.");	
 		}
 	);
 	
 	log( "pong-io", "pongIoUpdateData end.");
+}
+
+function pongIOrenderData( divId,  dta ) {
+	log( "pong-io", "pongIOrenderData data start");
+	var pmd = moduleConfig[ divId ];
+	var ctx = document.getElementById( divId+'Canvas' ).getContext("2d");
+	if ( pmd.io && pmd.io.length ) {
+		log( "pong-io", "IO: "+ pmd.io.length);			
+		for ( var c = 0; c < pmd.io.length; c++ ) {
+			var io = pmd.io[c];
+			log( "pong-io", io.type+' '+io.id );			
+			var ioDta = null;
+			if ( dta && dta[ io.id ] ) {
+				ioDta = dta[ io.id ];
+			}
+			if ( io.type && io.pos && io.pos.x && io.pos.y ) {
+				
+				if ( io.type == 'LED') {
+					pongIOrenderLED( ctx, io, ioDta );
+				} else if ( io.type == 'Switch') {
+					pongIOrenderSwitch( divId, ctx, io, ioDta );
+				} else if ( io.type == 'Poti') {
+					pongIOrenderPoti( divId, ctx, io, ioDta );
+				} else if ( io.type == 'Gauge') {
+					pongIOrenderGauge( ctx, io, ioDta );
+				} else if ( io.type == 'Display') {
+					pongIOrenderDisplay( ctx, io, ioDta );
+				} else if ( io.type == 'Graph') {
+					pongIOrenderGraph( ctx, io, ioDta );
+				} else if ( io.type == 'Img') {
+					pongIOrenderImg( ctx, io, ioDta );
+				}  
+			}
+		}		
+	}
+	log( "pong-io", "pongIOrenderData data end");
 }
 
 function pongIOrenderGauge( ctx, def, dta ) {
@@ -129,14 +225,21 @@ function pongIOrenderGauge( ctx, def, dta ) {
 	log( "pong-io", "pongIOrenderGauge end.");
 }
 
-function pongIOrenderSwitch( ctx, def, dta ) {
+function pongIOswitchClick( def, x, y ) {
+	log( "pong-io", "pongIOgetSwitchJS ("+x+"/"+y+")  "+JSON.stringify(def) );
+	// TODO
+	log( "pong-io", "pongIOgetSwitchJS: end" );
+}
+
+
+function pongIOrenderSwitch( divId, ctx, def, dta ) {
 	log( "pong-io", "pongIOrenderSwitch '"+def.id+"': "+JSON.stringify(dta) );
-	// TODO IO: implement switch
+	ioSense[ divId ][ def.id ] = new Array();
 	var x = parseInt( def.pos.x );
 	var y = parseInt( def.pos.y );
 	ctx.font = "14px Courier";
 	ctx.textAlign = "start";
-	ctx.textBaseline = "middle"; 
+	ctx.textBaseline = "middle";
 	if ( def.values && def.values.length ) { 
 		ctx.beginPath();
 		ctx.strokeStyle = "#00F";
@@ -151,58 +254,38 @@ function pongIOrenderSwitch( ctx, def, dta ) {
 			var yy = y - 8;
 			ctx.fillStyle   = "#00F";
 			ctx.fillText( def.values[0], xx, yy );
+			pongIOaddSwitchSense( divId, def.id, def.values[0], xx, yy );
 			if ( dta && dta.value && def.values[0] == dta.value ) {
-				var xt = x+10, yt = y-10;
-				log( "pong-io", xt +" / " + yt );
-				ctx.strokeStyle = "#00F";
-				ctx.fillStyle   = "#DDD";
-				ctx.lineTo( xt, yt ); 
-				ctx.stroke();
+				pongIOrenderSwitchLine( ctx, x+10, y-10 );
 			}
 			yy = y + 8;
 			ctx.fillStyle   = "#00F";
 			ctx.fillText( def.values[1], xx, yy );
+			pongIOaddSwitchSense( divId, def.id, def.values[1], xx, yy );
 			if ( dta && dta.value && def.values[1] == dta.value ) {
-				var xt = x+10, yt = y+10;
-				log( "pong-io", xt +" / " + yt );
-				ctx.strokeStyle = "#00F";
-				ctx.fillStyle   = "#DDD";
-				ctx.lineTo( xt, yt ); 
-				ctx.stroke();
+				pongIOrenderSwitchLine( ctx, x+10, y+10 );
 			}
 		} else if ( def.values.length == 3 ) {
 			var yy = y - 12;
 			ctx.fillStyle   = "#00F";
 			ctx.fillText( def.values[0], xx, yy );
+			pongIOaddSwitchSense( divId, def.id, def.values[0], xx, yy );
 			if ( dta && dta.value && def.values[0] == dta.value ) {
-				var xt = x+10, yt = y-10;
-				log( "pong-io", xt +" / " + yt );
-				ctx.strokeStyle = "#00F";
-				ctx.fillStyle   = "#DDD";
-				ctx.lineTo( xt, yt ); 
-				ctx.stroke();
+				pongIOrenderSwitchLine( ctx, x+10, y-10 );
 			}
 			yy = y;
 			ctx.fillStyle   = "#00F";
 			ctx.fillText( def.values[1], xx, yy );
+			pongIOaddSwitchSense( divId, def.id, def.values[1], xx, yy );
 			if ( dta && dta.value && def.values[1] == dta.value ) {
-				var xt = x+15, yt = y;
-				log( "pong-io", xt +" / " + yt );
-				ctx.strokeStyle = "#00F";
-				ctx.fillStyle   = "#DDD";
-				ctx.lineTo( xt, yt ); 
-				ctx.stroke();
+				pongIOrenderSwitchLine( ctx, x+15, y );
 			}
 			yy = y + 12;
 			ctx.fillStyle   = "#00F";
 			ctx.fillText( def.values[2], xx, yy );
+			pongIOaddSwitchSense( divId, def.id, def.values[2], xx, yy );
 			if ( dta && dta.value && def.values[2] == dta.value ) {
-				var xt = x+10, yt = y-10;
-				log( "pong-io", xt +" / " + yt );
-				ctx.strokeStyle = "#00F";
-				ctx.fillStyle   = "#DDD";
-				ctx.lineTo( xt, yt ); 
-				ctx.stroke();
+				pongIOrenderSwitchLine( ctx, x+10, y+10 );
 			}
 		}
 	}
@@ -210,7 +293,24 @@ function pongIOrenderSwitch( ctx, def, dta ) {
 	log( "pong-io", "pongIOrenderSwitch end.");
 }
 
-function pongIOrenderPoti( ctx, def, dta ) {
+function pongIOrenderSwitchLine( ctx, xt, yt ) {
+	log( "pong-io", xt +" / " + yt );
+	ctx.strokeStyle = "#00F";
+	ctx.fillStyle   = "#DDD";
+	ctx.lineTo( xt, yt ); 
+	ctx.stroke();	
+}
+
+function pongIOaddSwitchSense( divId, id, val, xx, yy ) {
+	log( "pong-io", "pongIOaddSwitchSense" );
+	ioSense[ divId ][ id ][ val ] = new Object();
+	ioSense[ divId ][ id ][ val ].x1 = xx;
+	ioSense[ divId ][ id ][ val ].x2 = xx + 30;
+	ioSense[ divId ][ id ][ val ].y1 = yy - 7;
+	ioSense[ divId ][ id ][ val ].y2 = yy + 7;	
+}
+
+function pongIOrenderPoti( divId, ctx, def, dta ) {
 	log( "pong-io", "pongIOrenderPoti '"+def.id+"': "+JSON.stringify(dta) );
 	var x = parseInt( def.pos.x );
 	var y = parseInt( def.pos.y );
@@ -224,6 +324,13 @@ function pongIOrenderPoti( ctx, def, dta ) {
 	ctx.rect( x, y, w, 20 );
 	ctx.stroke();
 	ctx.fill();  	
+	ioSense[ divId ][ def.id ] = new Object();
+	ioSense[ divId ][ def.id ].x1 = x;
+	ioSense[ divId ][ def.id ].x2 = x + w;
+	ioSense[ divId ][ def.id ].y1 = y;
+	ioSense[ divId ][ def.id ].y2 = y + 20;
+	ioSense[ divId ][ def.id ].min = min;
+	ioSense[ divId ][ def.id ].max = max;
 	if ( dta && dta.value ) {
 		log( "pong-io", "pongIOrenderPoti "+x+" + "+w+" * "+dta.value+" / ( "+max+" -"+ min+" )" );
 		ctx.beginPath();
@@ -238,6 +345,7 @@ function pongIOrenderPoti( ctx, def, dta ) {
 	}
 	log( "pong-io", "pongIOrenderPoti end.");
 }
+
 
 function pongIOrenderDisplay( ctx, def, dta ) {
 	log( "pong-io", "pongIOrenderDisplay '"+def.id+"': "+JSON.stringify(dta) );
--- html/svc/io/index.php
+++ html/svc/io/index.php
@@ -1,22 +1,105 @@
 <?php 
 header('Content-type: application/json');
+session_start();
+if ( ! isset( $_SESSION["p1"] ) ) {
+	$_SESSION["p1"] = 100;
+	$_SESSION["p2"] = 100;
+	$_SESSION["switchMain"] = 'Power ON';
+	$_SESSION["switchTMP"]  = 'OFF';
+	$_SESSION["ledTMP"]     = '-1';
+	$_SESSION["TMP"]     = '-';
+	$_SESSION["switchRP"]   = 'OFF';
+	$_SESSION["ledRP"]      = '-1';
+	$_SESSION["switchValveInflate"] = 100;
+	$_SESSION["switchValveN2n"] = 0;
+	
+	$_SESSION["switchValveN2"]   = 'close';
+	$_SESSION["switchValveTMP"]  = 'close';
+	$_SESSION["switchValveChHi"] = 'close';
+	$_SESSION["switchValveChLo"] = 'close';
+	
+	$_SESSION["ledValveN2"] = '-1';
+	$_SESSION["ledValveTMP"] = '-1';
+	$_SESSION["ledValveChHi"] = '-1';
+	$_SESSION["ledValveChLo"] = '-1';
+	
+}
+
+if ( $_SESSION["ledTMP"] == '2' ) {
+	if ( $_SESSION["switchTMP"]  == 'OFF') { // Power down done
+		$_SESSION["ledTMP"] = '-1';
+	} else {
+		if ( $_SESSION["switchRP"]  == 'ON') { // Power up done if PR is ON
+			$_SESSION["ledTMP"] = '1';
+		}
+	}
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	
+	error_log( $_POST['id'] .'='. $_POST['value'] );	
+	$_SESSION[ $_POST['id'] ] = $_POST['value'];
+	
+	if ( $_POST['id'] == 'switchTMP' ) {
+		if ( $_POST['value'] == 'ON' ) {
+			$_SESSION["ledTMP"] = '2';
+		} else {
+			$_SESSION["ledTMP"] = '2';
+		}
+	}
+	if ( $_POST['id'] == 'switchRP' ) {
+		if ( $_POST['value'] == 'ON' ) {
+			$_SESSION["ledRP"] = '1';
+		} else {
+			$_SESSION["ledRP"] = '-1';
+		}
+	}
+	if ( $_POST['id'] == 'switchValveN2' ) {
+		if ( $_POST['value'] == 'close' ) {
+			$_SESSION["ledValveN2"] = '-1';
+		} else {
+			$_SESSION["ledValveN2"] = '1';
+		}
+	}
+	if ( $_POST['id'] == 'switchValveTMP' ) {
+		if ( $_POST['value'] == 'close' ) {
+			$_SESSION["ledValveTMP"] = '-1';
+		} else {
+			$_SESSION["ledValveTMP"] = '1';
+		}
+	}
+	if ( $_POST['id'] == 'switchValveChHi' ) {
+		if ( $_POST['value'] == 'V3' ) {
+			$_SESSION["ledValveChHi"] = '-1';
+			$_SESSION["ledValveChLo"] = '1';
+		} else if ( $_POST['value'] == 'V4' ) {
+			$_SESSION["ledValveChLo"] = '-1';
+			$_SESSION["ledValveChHi"] = '1';
+		} else {
+			$_SESSION["ledValveChLo"] = '-1';
+			$_SESSION["ledValveChHi"] = '-1';
+		}
+	}
+}
+
 $data = array();
-$data['switchMain'] = array( 'value' => 'Power ON' );
-$data['ledValveN2']   = array( 'value' => '0' );
-$data['ledValveTMP']  = array( 'value' => '-1' );
-$data['ledValveChHi'] = array( 'value' => '-1' );
-$data['ledValveChLo'] = array( 'value' => '-1' );
-$data['ledTMP'] = array( 'value' => '-1' );
-$data['ledRP'] = array( 'value' => '1' );
-$data['displayG1'] = array( 'value' => '100 kPa' );
-$data['displayG2'] = array( 'value' => 'Hi' );
-$data['switchRP'] = array( 'value' => 'OFF' );
-$data['switchTMP'] = array( 'value' => 'OFF' );
-$data['switchValveN2'] = array( 'value' => 'close' );
-$data['switchValveTMP'] = array( 'value' => 'close' );
-$data['switchValveChHi'] = array( 'value' => 'close' );
-$data['switchValveChLo'] = array( 'value' => 'close' );
-$data['switchValveN2n'] = array( 'value' => '0' );
-$data['switchValveInflate'] = array( 'value' => '100' );
+$data['switchMain'] = array( 'value' => $_SESSION["switchMain"] );
+$data['ledValveN2']   = array( 'value' => $_SESSION["ledValveN2"] );
+$data['ledValveTMP']  = array( 'value' => $_SESSION["ledValveTMP"] );
+$data['ledValveChHi'] = array( 'value' => $_SESSION["ledValveChHi"] );
+$data['ledValveChLo'] = array( 'value' => $_SESSION["ledValveChLo"] );
+$data['ledTMP'] = array( 'value' => $_SESSION["ledTMP"] );
+$data['ledRP'] = array( 'value' => $_SESSION["ledRP"] );
+$data['displayG1'] = array( 'value' => $_SESSION["p1"].' kPa' );
+$data['displayG2'] = array( 'value' => $_SESSION["p1"].' kPa' );
+$data['switchRP']  = array( 'value' => $_SESSION["switchRP"] );
+$data['switchTMP'] = array( 'value' => $_SESSION["switchTMP"] );
+$data['switchValveN2']   = array( 'value' => $_SESSION["switchValveN2"]  );
+$data['switchValveTMP']  = array( 'value' => $_SESSION["switchValveTMP"]  );
+$data['switchValveChHi'] = array( 'value' => $_SESSION["switchValveChHi"]  );
+$data['switchValveChLo'] = array( 'value' => $_SESSION["switchValveChLo"]  );
+$data['switchValveN2n']     = array( 'value' => $_SESSION["switchValveN2n"] );
+$data['switchValveInflate'] = array( 'value' => $_SESSION["switchValveInflate"] );
 echo json_encode( $data , JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES  );
 ?>
