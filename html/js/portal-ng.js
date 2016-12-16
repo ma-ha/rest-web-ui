1f4eec2cdd640f10888540ba1eea17852c75c9c3
mh <m.e.harms@t-online.de>
2016-12-16 08:39:21 +0100

    v0.9.2

--- html/js/portal-ng.js
+++ html/js/portal-ng.js
@@ -30,7 +30,7 @@ THE SOFTWARE.
  former: Portal-NG (PoNG) http://mh-svr.de/mw/index.php/PoNG
 */
 var labeldefs = new Array();
-var PONGVER = '0.9.1';
+var PONGVER = '0.9.2';
 labeldefs['PONGVER'] = PONGVER;
 
 var moduleMap = {};
