590d16645de3645c66a3b35af41ab5d624ccace8
mh <m.e.harms@t-online.de>
2016-12-29 11:29:42 +0100

    IO graph: demo

--- html/svc/mock/arduino/index.php
+++ html/svc/mock/arduino/index.php
@@ -3,6 +3,24 @@ header('Content-type: application/json');
 $data = array();
 $data['myFirstLED'] = array( 'value' => '0' );
 $data['mySwitch'] = array( 'value' => 'off' );
+
+$dta =  array();
+
+$t = ( time() - 60*60 ) * 1000; 
+
+for ( $i=0; $i<60; $i++ ) {
+	$val = array();
+	$val[0] = $t;
+	$t += 60000;
+	$val[1] = $i/11;
+	$dta[] = $val;
+}
+
+$data['voltage'] = 
+	array(
+		array( 'name' => 'AD0', 'data' => $dta ),
+	);
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	$_POST = json_decode(file_get_contents('php://input'), true);
 	if ( $_POST['value'] == 'on' ) {
