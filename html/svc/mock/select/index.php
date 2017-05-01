e540c4bc183b8e27acfb79d7c4b61f128e7067be
mh <m.e.harms@t-online.de>
2017-05-01 18:06:45 +0200

    form: select example

--- html/svc/mock/select/index.php
+++ html/svc/mock/select/index.php
@@ -1,8 +1,8 @@
 <?php 
 $p = array();
-$p[] = array( 'name'=>'Red '. $_GET['f07'] );
-$p[] = array( 'name'=>'Green '. $_GET['f07'] );
-$p[] = array( 'name'=>'Blue '. $_GET['f07'] );
+$p[] = array( 'name'=>'Red '. $_GET['cat'] );
+$p[] = array( 'name'=>'Green '. $_GET['cat'] );
+$p[] = array( 'name'=>'Blue '. $_GET['cat'] );
 header('Content-type: application/json');
 echo json_encode( $p , JSON_PRETTY_PRINT );
 ?>
